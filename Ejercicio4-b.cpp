4093ef43d0bf8e9e8d5c2dab6dbcd20e1c1a2192
leydermejia <30670884+leydermejia@users.noreply.github.com>
2018-03-19 18:25:49 -0500

    Add files via upload

---
+++ Ejercicio4-b.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include <math.h>
+/*Comentario
+Estructura Estudiantes, en la cual se tendran los siguientes
+campos:Nombre,edad,grado,promedio.Pedir datos al usuario para 3 alumnos
+comprobar cual de los 3 tiene el mejor promedio y imprimir los datos del
+alumno
+Elaborado por: Fernando Lopez Mejia, Leyder Londoño Mejia*/
+struct Estudiantes{
+	char nombre[30];	
+	int edad;
+	float grado;
+	float promedio;	
+}alumnos[3], *pAlumnos=alumnos;
+void referencia(Estudiantes v[]);
+Estudiantes v[100];
+int main() {
+	referencia(v);
+	return 0;
+}
+void referencia(Estudiantes v[]){
+	int i,lugar;
+	float MayorNota=0.0;
+	
+	for(i=0;i<3;i++){
+		printf("\n Nombre \n");
+		fflush(stdin);
+		fgets((pAlumnos+i)-> nombre,30,stdin);
+		printf("Edad \n");
+		scanf("%d",&(pAlumnos+i)->edad);
+		printf("Grado \n");
+		scanf("%f",&(pAlumnos+i)->grado);
+		printf("Promedio \n");
+		scanf("%f",&(pAlumnos+i)->promedio);
+		printf("--------- \n -----------\n");
+	}
+	
+	for(i=0;i<3;i++){
+		if((pAlumnos+i)->promedio > MayorNota){
+			MayorNota=((pAlumnos+i)->promedio);
+			lugar=i;
+		}
+	}
+	printf("Datos del estudiante con mejor promedio es \n");
+	printf("Nommbre %s \n",pAlumnos->nombre);
+	printf("Nommbre %d \n",pAlumnos->edad);
+	printf("Nommbre %.2f \n",pAlumnos->grado);
+	printf("Nommbre %.2f \n",pAlumnos->promedio);
+	
+}
+
+
---
+++ Ejercicio4-c.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include <math.h>
+/*Comentario
+Programa que lee en un arreglo de estructuras los datos
+de N empleados de la empresa y que imprima los datos del empleado con
+mayor y menor salario.
+Elaborado por:Fernando Lopez Mejia,Leyder Londoño Mejia*/
+struct empleado{
+	char nombre[20];
+	char sexo[20];
+	float salario;	
+}empleados[100], *pempleados=empleados;
+
+void referencia(empleado v[],int longt);
+
+int main() 
+{	
+	empleado v[100];
+	int n;
+	referencia(v,n);
+	return 0;
+}
+
+void referencia(empleado v[],int longt){
+	int i,SalarioM,SalarioMe;
+	float Alto = 0.0, Bajo = 9999999;
+	printf("Ingrese Cantidad de empleados \n ");
+	scanf("%d",&longt);
+	
+	for(i=0;i<longt;i++){
+		fflush(stdin);
+		printf("Nombre Empleado \n ");
+		fgets((pempleados+i)->nombre,20,stdin);
+		fflush(stdin);
+		printf("Sexo Empleado \n ");
+		fgets((pempleados+i)->sexo,20,stdin);
+		printf(" Salario del Empleado \n");
+		scanf("%f",&(pempleados+i)->salario);
+		printf("----------\n------------\n");
+	}
+	for(i=0;i<longt;i++){
+		if((pempleados+i)->salario > Alto){ 
+			Alto= ((pempleados+i)->salario);
+			SalarioM = i;
+		}
+		else if((pempleados+i)->salario < Bajo){
+			Bajo = ((pempleados+i)->salario);
+			SalarioMe = i;
+		}
+	}
+	
+	printf("\n El Empleado con el mayor salario es \n");
+	printf("\n Nombre %s",(pempleados+i)->nombre);
+	printf("\n Sexo %s",(pempleados+i)->sexo);
+	printf("\n Salario: %.2f",(pempleados+i)->salario);
+	
+	printf("\n El Empleado con el menor salario es: \n");
+	printf("\n Nombre %s",(pempleados+i)->nombre);
+	printf("\n Sexo %s",(pempleados+i)->sexo);
+	printf("\n Salario %.2f \n",(pempleados+i)->salario);
+	
+}
+
