764ce1f5442e9e91d874f6bc26022998af74da7e
leydermejia <30670884+leydermejia@users.noreply.github.com>
2018-03-19 19:10:33 -0500

    Add files via upload

---
+++ Ejercicio4-e.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include <math.h>
+/*Comentario
+Utilizar las 2 estructuras del ejercicio anterior, pero ahora pedir los datos para N 
+alumnos, y calcular cual de todos tiene el mejor promedio y menor, e imprimir sus datos.
+Elaborado por:Fernando Lopez Mejia,Leyder Londoño Mejia*/
+struct promedio{
+	float nota1;
+	float nota2;
+	float nota3;
+};
+
+struct alumno{
+	char nombre[20];
+	char sexo[20];
+	int edad;
+	struct promedio prom;
+}alumnos[100],*palumnos=alumnos;
+
+void estudiantes(alumno v[],int longt);
+
+int main()
+{
+	alumno v[100];
+	int longt;
+	estudiantes(v,longt);
+	return 0;
+}
+
+void estudiantes(alumno v[],int longt){
+	int i,PromedioM;
+	float promedio[100], mayor = 0.0;
+	printf(" Ingrese Cantidad de Alumnos ");
+	scanf("%i",&longt);	
+	for(i=0;i<longt;i++){
+		printf(" Nombre \n");
+		fflush(stdin);
+		fgets((palumnos)-> nombre,30,stdin);
+		printf(" Sexo \n ");
+		fgets((palumnos)-> sexo,30,stdin);
+		printf(" Edad \n");
+		scanf("%d",&(palumnos)->edad);
+		printf(" Notas \n");
+		scanf("%f %f %f",&(palumnos)->prom.nota1,&(palumnos)->prom.nota2,&(palumnos)->prom.nota3);
+		printf("-----\n-------\n");		
+		promedio[i]= ((palumnos)->prom.nota1+(palumnos)->prom.nota2+(palumnos)->prom.nota3/3);	
+		if((promedio[i] > mayor){
+			mayor = promedio[i];
+			PromedioM=i;
+		}
+}	
+	printf("\n------*Alumno con Mayor Promedio*-------\n");
+	printf("\n Nombre %s",(palumnos)->nombre);
+	printf("\n Sexo %s",(palumnos)->sexo);
+	printf("\n Edad %i",(palumnos)->edad);
+	printf("\n Promedio %.2f\n",promedio);
+}
+
---
+++ Ejercicio4-f.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include <math.h>
+/*Comentario
+Defina una estructura que sirva para representar una persona la estructura debe 
+contener dos campos: el nombre de la persona y un valor de tipo logico que indica si 
+la persona tiene algun tipo de discapacidad. Realice un programa que dado un vector 
+de personas rellene dos nuevos vectores: uno que contenga las personas que no tienen
+ningun tipo de discapacidad y otro que contenga las personas con discapacidad.
+Elaborado por: Leyder Londoño Mejia*/
+struct personas{
+	char nombre[30];
+	bool discapacidad;
+}persona[3],*ppersona=persona;
+
+int disc = 0, sinDisc = 0;
+void personitas(personas v[],int tam);
+
+int main()
+{
+	personas v[100];
+	int tam;
+	personitas(v,tam);
+	return 0;
+}
+
+void personitas(personas v[],int tam)
+{
+	int i,opcion;
+	printf("Ingrese Cantidad de personas:\n");
+	scanf("%d",&tam);
+	for (i=0;i<tam;i++){
+		printf("\n Nombre: \n",i+1);
+		fflush(stdin);
+		fgets((ppersona)-> nombre,30,stdin);
+		printf(" Tiene alguna discapacidad ");
+		printf("Ingrese el numero 1.Si ó 2.No\n");
+		scanf("%d",&opcion);
+		switch(opcion){
+		case 1:
+			(ppersona)->discapacidad=true;
+			break;
+		case 2:
+			(ppersona)->discapacidad=false;
+			break;
+		}
+	}
+	printf("\n Personas con discapacidad \n");
+	for(i=0;i<tam;i++){		
+		if((ppersona)->discapacidad == true){
+			disc=i;
+			printf("%s \n",(ppersona)->nombre);
+		}
+	}
+	printf("\n Personas SIN discapacidad \n");
+	for(i=0;i<tam;i++){		
+		if((ppersona)->discapacidad==false){
+			sinDisc=i;
+			printf("%s \n",(ppersona)->nombre);
+		}
+	}
+}
+
