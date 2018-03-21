57f2dcd7953e193a81bd42f49265a7be5268f384
leydermejia <30670884+leydermejia@users.noreply.github.com>
2018-03-20 21:02:26 -0500

    Add files via upload

---
+++ Ejercicio1.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+/*Comentario
+VECTOR QUE ME LEA 100 NUMEROS Y QUE ME HAGA LA SUMMA,
+ DE LOS PARES Y LOS IMPARES*/
+int main() {
+	int i,suma=0;
+	int impar=0;
+	int *num;
+	int Vnum[100];
+	num=new int[100];
+	printf("Numeros del vector \n");
+	for(i=0;i<100;i++){
+	Vnum[i]=i;
+		printf("\n Los numeros del vector son: %i",Vnum[i]);
+			if(i%2==0){
+				suma=suma+i;
+			}else{
+				impar=impar+i;
+			}
+		}
+		printf("\n La suma de los 100 numeros pares es: %i",suma);
+			
+	printf("\n La suma de los 100 numeros impares es: %i",impar);
+	delete[]num;
+	return 0;
+}
---
+++ Ejercicio2.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+/*LEER 4 NUMEROS ORDENARLOS DE MAYOR A MENOR Y DIGA,
+ CUAL ES EL MAYOR Y CUAL EL MENOR*/
+int vector[100],num,aux;
+int main() {
+	int i,*num;
+	int j;
+	int aux,bandera=0;
+	num=new int[4];
+	
+	printf("Ingrese los elementos del vector \n");
+	for(i=0;i<4;i++){
+		printf("Ingrese los 4 numeros \n ");
+		scanf("%d",&num[i]);
+	}
+	printf("Elementos ordenados \n");
+	for(i=0;i<4;i++){
+		bandera=1;
+		for(j=0;j<4;j++){
+			if(num[j]<num[bandera]){
+				aux=num[j];
+				num[j]=num[bandera];
+				num[bandera]=aux;
+				
+			}
+			bandera=bandera+1;
+			
+		}
+	}
+	for(i=0;i<4;i++){
+		printf("%i",num[i]);
+		
+	}
+	printf("El mayor es: %i\n",num[3]);
+	printf("El menor es: %i\n",num[0]);
+	delete[]num;
+	return 0;
+}
+
